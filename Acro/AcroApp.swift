b9d3af9044f0803d1318786278ea91ca6d087f35
Abdulk-A <abdullahabdulkareem273@gmail.com>
2025-10-02 00:46:29 -0500

    Initial Commit

---
+++ Acro/AcroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcroApp.swift
+//  Acro
+//
+//  Created by Abdullah on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AcroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
